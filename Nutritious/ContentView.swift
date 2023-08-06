1963d7bb43ed4689d3584d8128541a5834941516
sophcheng <sophiecheng181@gmail.com>
2023-08-06 15:42:33 -0400

    Initial Commit

---
+++ Nutritious/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nutritious
+//
+//  Created by Sophie Cheng on 8/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nutritious/NutritiousApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritiousApp.swift
+//  Nutritious
+//
+//  Created by Sophie Cheng on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritiousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
